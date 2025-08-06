4dfd71785b9be75bb8fa1bb26f672106d54819b9
Kalynn Willis <khwillis@ad.wisc.edu>
2025-08-06 12:57:16 -0500

    working tool system

--- rag/model.py
+++ rag/model.py
@@ -21,13 +21,59 @@ class Model:
         self.model_name = model_name
 
     def chat(self, messages: List[Message], tools: dict = None):
-        response: ChatResponse = chat(model=self.model_name, messages=messages, tools=tools)
-        return response.message.content
+        response: ChatResponse = chat(model=self.model_name, messages=messages, tools=tools, think=False)
+        return response.message
 
 
 if __name__ == "__main__":
-    model = Model("phi3:mini")
-    messages = [{"role": "user", "content": "Why is the sky blue?"}]
-    print(model.chat(messages))
-    
+    model = Model("qwen3:8b")
+    def add_numbers(num1: int, num2: int):
+        """Adds two numbers together
+        Args:
+            num1: int
+            num2: int
+        Returns:
+            int: The sum of the two numbers
+        """
+        return num1 + num2
+    messages = [{"role": "user", "content": "What is 4+4?"}]
+    response = model.chat(messages, tools=[add_numbers])
+    print(messages)
+    messages.append(response)
+    tool_args = response.tool_calls[0].function.arguments
+    tool_output = add_numbers(**tool_args)
+    print(messages)
+    messages.append({"role": "tool", "content": str(tool_output)})
+    response = model.chat(messages, tools=[add_numbers])
+    print(messages)
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+[{"role": "user", "content": "What is 4+4?", "tools": [add_numbers]}, 
+ {"role": "assistant", "content": "", "tool_calls": [{"name": "add_numbers", "args": {"num1": 4, "num2": 4}}]},
+ {"role": "tool", "content": "8"},
+ {
+    "role": "assistant",
+    "content": "8",
+    "tool_calls": [
+        {
+            "name": "add_numbers",
+            "args": {"num1": 4, "num2": 4}
+        }
+    ]
+}]
--- rag/tools.py
+++ rag/tools.py
@@ -3,13 +3,6 @@
 
 #from db import connector
 
-#def get_gene_info(gene_id: str):
-    #pass
-
-#def get_highest_lod_genes(trait_class: str):
-    #query = f"SELECT * FROM gene_info WHERE trait_class = '{trait_class}' ORDER BY lod_score DESC LIMIT 10"
-    #response = connector.query(query)
-    #return response
 
 def add_numbers(num1: int, num2: int):
     return num1 + num2
